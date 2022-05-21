9d8b5d19cb4e63d15a6729d49f24e25cf99c1a86
u7085556 <mrsecretjoker@gmail.com>
2022-05-21 14:24:40 +0800

    Change TH and voting rule for apply private set

--- voting.py
+++ voting.py
@@ -7,11 +7,13 @@ import copy
 
 name = 'voting'
 model_path = {
-        'U+_nc_ef4ap_FTL_10fd0':1,
-        'U+_nc_ef4ap_FTL_10fd4':1,
-        'U+_nc_ef4ap_FTL_10fd5':1,
-        'U+_nc_ef4ap_FTL_10fd6':1,
-        'U+_nc_ef4ap_FTL_10fd7':1,
+        'U+_nc_ef4ap_sDL_10fd0':1,
+        'U+_nc_ef4ap_sDL_10fd3':1,
+        'U+_nc_ef4ap_sDL_10fd4':1,
+        'U+_nc_ef4ap_sDL_10fd5':1,
+        'U+_nc_ef4ap_sDL_10fd6':1,
+        'U+_nc_ef4ap_sDL_10fd7':1,
+        'U+_nc_ef4ap_sDL_10fd8':1,
         }
 height = 942
 width = 1716
@@ -33,13 +35,13 @@ if __name__ == "__main__":
         for infpth in model_path:
             infmask = cv2.imread(os.path.join(infpth, image_id), 0)
             infmask = np.where(infmask != 0, 1, 0)
-            if np.sum(infmask) > 350:
-                mask += (infmask * model_path[infpth])
-                vote += model_path[infpth]
+            rate = model_path[infpth] if np.sum(infmask) > 200 else model_path[infpth] / 2
+            mask += (infmask * rate)
+            vote += rate
         mask = np.where(mask > vote/2, 1, 0)
         mask_copy = copy.deepcopy(mask)
-        connectTH(mask_copy, mask_copy, mode=1, threshold=400)
-        if np.sum(mask_copy) > 400:
+        connectTH(mask_copy, mask_copy, mode=1, threshold=150)
+        if np.sum(mask_copy) > 150:
             mask = mask_copy
         connectTH(mask, mask^1, mode=0, threshold=50000)
         cv2.imwrite(os.path.join(Public_save_path, image_id), mask*255)
